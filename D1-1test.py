9f272b0ad918d9debba08374d4afed1f3d36ba56
henry0727 <78331078+henry0727@users.noreply.github.com>
2021-02-01 10:47:22 +0800

    Add files via upload

---
+++ D1-1test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:43:23 2021
+
+@author: MTAEXAM
+"""
+
+top=float(input("請輸入上底"))
+bot=float(input("請輸入下底"))
+h=float(input("請輸入高"))
+area=(top+bot)*h/2
+print("梯形面積是"+str(area))
+5
